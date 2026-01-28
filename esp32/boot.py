82c00708f9f33119d5ec070f868d6cf6b0e364a9
up-44 <155664923+up-44@users.noreply.github.com>
2026-01-28 14:52:49 +0100

    Comments added for better understanding

--- esp32/boot.py
+++ esp32/boot.py
@@ -1,21 +1,21 @@
 import network
 import time
-import secrets
+import secrets # importiert Inhalt aus secrets.py
 
-def do_connect():
+def do_connect(): # Funktion zum Verbinden mit dem WLAN
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
         print('Verbinde mit Netzwerk...')
         wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASS)
         
-        # Versuche 10 Sekunden lang zu verbinden
+        # 10 Sekunden lang zu verbinden
         attempt = 0
         while not wlan.isconnected() and attempt < 10:
             time.sleep(1)
             attempt += 1
             
-    if wlan.isconnected():
+    if wlan.isconnected(): # Fehler IF-Schleife
         print('WLAN verbunden! Netzwerk-Konfiguration:', wlan.ifconfig())
     else:
         print('WLAN Verbindung fehlgeschlagen!')
